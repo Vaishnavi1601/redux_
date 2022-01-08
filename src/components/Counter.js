d401fc70225f88bbdf55fc2f542a42cd9c094782
Vaishnavi1601 <vaishnavi7734@gmail.com>
2022-01-08 16:02:18 +0530

    working with multiple state properties

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,11 @@
 import { useSelector,useDispatch } from "react-redux";
 //useSelector allows us to then automatically select a part of our state managed by the store
-
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter); //a function which determines which piece of data we want to extract from our store
+  const show = useSelector((state) => state.showCounter); //a function which determines which piece of data we want to extract from our store
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -16,12 +16,15 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
-  const toggleCounterHandler = () => {};
+
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'});
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increment by 10</button>
--- src/store/index.js
+++ src/store/index.js
@@ -5,22 +5,36 @@ import { createStore} from 'redux';
 
 //counterReduce r gets "existing state as first argument", and action that was dispatched as the second argument
 //default value is given to state
-const counterReducer = (state = {counter:0}, action) =>{
+
+const inititalState = {counter:0, showCounter: true}
+const counterReducer = (state = inititalState, action) =>{
   if(action.type === 'increment') {
     return {
-      counter: state.counter + 1
+      counter: state.counter + 1,
+      showCounter: state.showCounter
     }
   }
 
   if(action.type === 'increase') {
     return {
-      counter: state.counter + action.amount
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter
+
     }
   }
 
   if (action.type === 'decrement') {
     return {
-      counter: state.counter - 1
+      counter: state.counter - 1,
+      showCounter: state.showCounter
+
+    }
+  }
+
+  if(action.type === 'toggle'){
+    return{
+      showCounter: !state.showCounter,
+      counter: state.counter
     }
   }
   // if  neither action is done return the unchanged state
