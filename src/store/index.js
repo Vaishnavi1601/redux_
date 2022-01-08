3af539639eb444c4e0390f39aa0e6d56bdf96ac3
Vaishnavi1601 <vaishnavi7734@gmail.com>
2022-01-08 12:21:19 +0530

    creating redux store

---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+//creating new store
+
+//importing createstore function
+import { createStore} from 'redux'; 
+
+//counterReducer gets "existing state as first argument", and action that was dispatched as the second argument
+//default value is given to state
+const counterReducer = (state = {counter:0}, action) =>{
+  if(action.type === 'increment') {
+    return {
+      ocunter: state.counter + 1
+    }
+  }
+
+  if (action.type === 'decremnt') {
+    return {
+      counter: state.counter - 1
+    }
+  }
+  // if  neither action is done return the unchanged state
+  return state;
+}
+
+
+//calling createStore and storing it in a constant store
+//this now creates our redux store
+const store = createStore(counterReducer);
+
+export default store;
